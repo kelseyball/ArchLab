bb3a25456f098880d513fb94f8f1231ad164e23f
Jianyu Huang <hjyahead@gmail.com>
2014-10-24 13:13:36 -0500

    finish asm and ucode4; need more modifications; going to code for lc3bsim4

---
+++ lab4/add.asm
@@ -0,0 +1,32 @@
+        .ORIG x3000
+        LEA R0, USPTR
+        LDW R6, R0, #0
+
+        AND R0, R0, #0
+        ADD R0, R0, #1
+        LEA R1, MOVLOC
+        LDW R1, R1, #0
+        STB R0, R1, #0
+
+        AND R0, R0, #0
+        ADD R0, R0, #20   ;R0: Counter
+        AND R1, R1, #0    ;R1: Sum
+        LEA R2, ADDLOC
+        LDW R2, R2, #0    ;R2: Intial Location
+ 
+LOOP    LDB R3, R2, #0
+        ADD R2, R2, #1
+        ADD R1, R1, R3
+        ADD R0, R0, #-1
+        BRp LOOP
+
+#.FILL   xBF00
+        LEA R2, SUMLOC
+        LDW R2, R2, #0
+        STW R1, R2, #0
+        HALT
+
+USPTR   .FILL xFE00
+MOVLOC  .FILL x4000
+ADDLOC  .FILL xC000
+SUMLOC  .FILL xC014
---
+++ lab4/vector_table.asm
@@ -0,0 +1,5 @@
+    .ORIG x0202
+    .FILL x1200
+    .FILL x1600
+    .FILL x1A00
+    .FILL x1C00
