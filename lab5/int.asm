0911766b63b530638e7451341566380474d1b94c
Jianyu Huang <hjyahead@gmail.com>
2014-11-14 09:32:27 -0600

    update page table, int.asm, vector table, ucode5

--- lab5/int.asm
+++ lab5/int.asm
@@ -3,19 +3,29 @@
         STW R0, R6, #0
         ADD R6, R6, #-2
         STW R1, R6, #0
+        ADD R6, R6, #-2
+        STW R2, R6, #0
 
-        LEA R0, ADDR
+        LEA R0, PGST
         LDW R0, R0, #0
-        LDW R1, R0, #0
-        ADD R1, R1, #1
-        STW R1, R0, #0
+        LEA R1, PLEN
+        LDW R1, R1, #0
 
+LOOP    LDW R2, R0, #0
+        AND R2, R2, #-2
+        STW R2, R0, #0
+        ADD R0, R0, #2
+        ADD R1, R1, #-1
+        BRp LOOP
+
+        LDW R2, R6, #0
+        ADD R6, R6, #2
         LDW R1, R6, #0
         ADD R6, R6, #2
         LDW R0, R6, #0
         ADD R6, R6, #2
-
         RTI
 
-ADDR    .FILL x4000
+PGST    .FILL x1000
+PLEN    .FILL x0080
         .END
